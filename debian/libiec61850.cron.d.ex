bcb47c9ab94acf2a390aacf2a30890289282c609
Robert Middleton <rmiddleton@synexxus.com>
2018-06-06 09:35:50 -0400

    Cleaned up debian files

--- debian/libiec61850.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libiec61850 package
-#
-0 4	* * *	root	[ -x /usr/bin/libiec61850_maintenance ] && /usr/bin/libiec61850_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libiec61850):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libiec61850" command="/usr/bin/libiec61850"
